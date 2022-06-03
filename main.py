608599bd8d51ab6acad37bf893adcada2c7caf3b
Ls3109 <louissenac09@gmail.com>
2022-06-03 02:27:07 +0200

    menue= done

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from Game import *
 import pygame
+import pygame_menu
 from Menu import *
+
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+def refresh_and_lunch():
+    print("im in")
+    game = Game()
+    game.start()
 
 
 def main():
-    game = Game()
-    game.start()
+    menu = pygame_menu.Menu('Welcome', 600, 400,
+                            theme=pygame_menu.themes.THEME_DARK)
+    menu.add.text_input('Name : ')
+    menu.add.button('Play', refresh_and_lunch)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    # game = Game()
+    # game.start()
+    menu.mainloop(surface)
 
 
 if __name__ == "__main__":
